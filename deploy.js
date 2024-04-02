6fa950bb335b7b36ded5103867483e6ca8a5633b
Tom Wilson <tom@hyper.io>
2024-04-02 21:59:41 +0000

    update with deploy example

---
+++ deploy.js
@@ -0,0 +1,33 @@
+// import Irys as normal
+import Irys from "@irys/sdk";
+import fs from "fs";
+
+// load Arweave wallet
+const wallet = JSON.parse(
+  fs.readFileSync(process.env.WALLET, 'utf-8')
+);
+
+const main = async () => {
+  const token = "arweave";
+  const irys = new Irys({
+    url: "https://turbo.ardrive.io", // URL of the node you want to connect to, https://turbo.ardrive.io will facilitate upload using ArDrive Turbo.
+    token, // Token used for payment and signing
+    key: wallet, // Arweave wallet
+  });
+
+  const receipt = await irys.uploadFile("./aos/process/process.wasm", [
+    { name: 'Content-Type', value: 'application/wasm' },
+    { name: 'Data-Protocol', value: 'ao' },
+    { name: 'Type', value: 'Module' },
+    { name: 'Variant', value: 'ao.TN.1' },
+    { name: 'Module-Format', value: 'wasm32-unknown-emscripten2' },
+    { name: 'Input-Encoding', value: 'JSON-1' },
+    { name: 'Output-Encoding', value: 'JSON-1' },
+    { name: 'Memory-Limit', value: '500-mb' },
+    { name: 'Compute-Limit', value: '9000000000000' },
+  ]);
+  console.log(receipt);
+  console.log('ModuleID: ', receipt.id)
+}
+
+main()
