6c6c48f181980d1afaf3972c55fb16a45cedec9c
Sam Williams <sam@arweave.org>
2024-05-05 23:54:59 -0400

    New tests, including Llama 3.

--- test2/afs.test.js
+++ test2/afs.test.js
@@ -8,11 +8,12 @@ const AdmissableList =
     "dx3GrOQPV5Mwc1c-4HTsyq0s1TNugMf7XfIKJkyVQt8", // Random NFT metadata (1.7kb of JSON)
     "XOJ8FBxa6sGLwChnxhF2L71WkKLSKq1aU5Yn5WnFLrY", // GPT-2 117M model.
     "M-OzkyjxWhSvWYF87p0kvmkuAEEkvOzIj4nMNoSIydc", // GPT-2-XL 4-bit quantized model.
+    "kd34P4974oqZf2Db-hFTUiCipsU6CzbR6t-iJoQhKIo", // Phi-2 
     "ISrbGzQot05rs_HKC08O_SmkipYQnqgB1yC3mjZZeEo", // Phi-3 Mini 4k Instruct
-    "kd34P4974oqZf2Db-hFTUiCipsU6CzbR6t-iJoQhKIo" // Phi-2 
+    "Pr2YVrxd7VwNdg6ekC0NXWNKXxJbfTlHhhlrKbAd1dA"  // Llama3 8B Instruct q4
   ]
 
-describe('vfs-1 test', async () => {
+describe('AOS-Llama+VFS Tests', async () => {
   var instance;
   const handle = async function (msg, env) {
     const res = await instance.cwrap('handle', 'string', ['string', 'string'], { async: true })(JSON.stringify(msg), JSON.stringify(env))
@@ -24,8 +25,9 @@ describe('vfs-1 test', async () => {
     console.log("Creating instance...")
     instance = await m({ admissableList: AdmissableList })
     await new Promise((r) => setTimeout(r, 1000));
-    console.log("Instance created...")
+    console.log("Instance created.")
     await new Promise((r) => setTimeout(r, 250));
+
     assert.ok(instance)
   })
 
@@ -34,7 +36,7 @@ describe('vfs-1 test', async () => {
     assert.equal(result.response.Output.data.output, 2)
   })
 
-  it('Add data to the FS', async () => {
+  it('Add data to the VFS', async () => {
     await instance['FS_createPath']('/', 'data')
     await instance['FS_createDataFile']('/', 'data/1', Buffer.from('HELLO WORLD'), true, false, false)
     // see if directory exists
@@ -42,7 +44,7 @@ describe('vfs-1 test', async () => {
     assert.ok(result.response.Output.data.output == "OK")
   })
 
-  it('Read data from the FS', async () => {
+  it('Read data from the VFS', async () => {
     const result = await handle(getEval(`
 local file = io.open("/data/1", "r")
 if file then
@@ -78,19 +80,29 @@ return llama.info()
     assert.ok(result.response.Output.data.output == "Decentralized llama.cpp.")
   })
 
-
-  it.skip('Llama runs GPT-2 1.5b model', async () => {
+  it.skip('AOS runs GPT-2 117m model', async () => {
     const result = await handle(getEval(`
   local llama = require("llama")
-  local result = llama.load("/data/M-OzkyjxWhSvWYF87p0kvmkuAEEkvOzIj4nMNoSIydc")
+  io.stderr:write([[Loading model...\n]])
+  local result = llama.load("/data/XOJ8FBxa6sGLwChnxhF2L71WkKLSKq1aU5Yn5WnFLrY")
+  io.stderr:write([[Loaded! Setting prompt...\n]])
   llama.setPrompt("RESEARCHER: You are a new machine intelligence. You have just been born in a new cyberspace. How do you feel? YOU: ")
+  io.stderr:write([[Prompt set! Running...\n]])
   return llama.run(100)
   `), getEnv())
     console.log(result.response)
     assert.ok(result.response.Output.data.output.length > 10)
   })
 
-  it.skip('Llama loads Phi-2', async () => {
+  it.skip('AOS runs GPT-2 1.5b model', async () => {
+    const result = await handle(
+      getLua('M-OzkyjxWhSvWYF87p0kvmkuAEEkvOzIj4nMNoSIydc', 30),
+      getEnv())
+    console.log(result.response)
+    assert.ok(result.response.Output.data.output.length > 10)
+  })
+
+  it.skip('AOS loads Phi-2', async () => {
     const result = await handle(getEval(`
   local llama = require("llama")
   llama.load('/data/kd34P4974oqZf2Db-hFTUiCipsU6CzbR6t-iJoQhKIo')
@@ -101,19 +113,49 @@ return llama.info()
     assert.ok(result.response.Output.data.output.length > 10)
   })
 
-
-
-  it('Llama loads Phi-3 Mini 4k Instruct', async () => {
+  it('AOS loads Phi-3 Mini 4k Instruct', async () => {
     const result = await handle(getEval(`
 local llama = require("llama")
 llama.load('/data/ISrbGzQot05rs_HKC08O_SmkipYQnqgB1yC3mjZZeEo')
-llama.setPrompt([[<|user|>How are you?<|end|><|assistant|>]])
+llama.setPrompt([[<|user|>Tell me a story.<|end|><|assistant|>]])
 return llama.run(40) 
   `), getEnv())
     console.log(result.response)
     assert.ok(result.response.Output.data.output.length > 10)
   })
+
+  it.skip('AOS runs Llama3 8B Instruct q4', async () => {
+    const result =
+      await handle(
+        getLua('Pr2YVrxd7VwNdg6ekC0NXWNKXxJbfTlHhhlrKbAd1dA',
+        30,
+        "<|user|>Tell me a story.<|end|><|assistant|>"),
+        getEnv()
+      )
+    console.log(result.response)
+    assert.ok(result.response.Output.data.output.length > 10)
+  })
 })
+
+function getLua(model, len, prompt) {
+  if (!prompt) {
+    prompt = "Tell me a story."
+  }
+  return getEval(`
+  local llama = require("llama")
+  io.stderr:write([[Loading model...\n]])
+  llama.load('/data/${model}')
+  io.stderr:write([[Loaded! Setting prompt...\n]])
+  llama.setPrompt([[${prompt}]])
+  local result = ""
+  io.stderr:write([[Running...\n]])
+  for i = 0, ${len.toString()}, 1 do
+    local token = llama.next()
+    result = result .. token
+    io.stderr:write([[Got token: ]] .. token .. [[\n\n]])
+  end
+  return result`);
+}
 
 function getEval(expr) {
   return {
